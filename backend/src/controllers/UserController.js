af8043b60a8ab0b9220b51ff0015266067f3d416
Bruno Sousa <brse01@gmail.com>
2020-03-13 14:08:09 -0300

    Tratamento de error

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -2,51 +2,77 @@ const User = require('../models/User');
 
 module.exports = {
   async index(req, res) {
-    const users = await User.findAll();
+    try {
+      const users = await User.findAll();
 
-    return res.json(users);
+      return res.json(users);
+    } catch (error) {
+      return res.status(500).send();
+    }
+
+
   },
   async show(req, res) {
-    const { id } = req.params;
-    const user = await User.findByPk(id);
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
 
-    if (user) {
-      return res.json(user);
-    } else {
-      return res.json({ Infor: `User with id ${id} does not exist` });
+      if (user) {
+        return res.json(user);
+      } else {
+        return res.json({ Infor: `User with id ${id} does not exist` });
+      }
+    } catch (error) {
+      return res.status(500).send();
     }
+
 
 
   },
   async update(req, res) {
-    const { first, last } = req.body;
-    const { id } = req.params;
 
-    let user = await User.findByPk(id);
-    if (user) {
-      user = await User.update({ first, last }, { returning: true, where: { id: id } });
-    } else {
-      return res.status(204).json({ Infor: `User with id ${id} does not exist` });
+    try {
+      const { first, last } = req.body;
+      const { id } = req.params;
+
+      let user = await User.findByPk(id);
+      if (user) {
+        user = await User.update({ first, last }, { returning: true, where: { id: id } });
+      } else {
+        return res.status(204).json({ Infor: `User with id ${id} does not exist` });
+      }
+
+      return res.json(user);
+    } catch (error) {
+      return res.status(500).send();
     }
 
-    return res.json(user);
+
 
   },
   async store(req, res) {
-    const { first, last } = req.body;
-    const user = await User.create({ first, last });
+    try {
+      const { first, last } = req.body;
+      const user = await User.create({ first, last });
 
-    return res.json(user);
+      return res.json(user);
+    } catch (error) {
+      return res.status(500).send();
+    }
   },
 
   async destroy(req, res) {
-    const { id } = req.params;
-    const user = await User.findByPk(id);
-    if (user) {
-      await user.destroy();
-      return res.status(204).json();
-    } else {
-      return res.json({ Infor: `User with id ${id} does not exist` });
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      if (user) {
+        await user.destroy();
+        return res.status(204).json();
+      } else {
+        return res.json({ Infor: `User with id ${id} does not exist` });
+      }
+    } catch (error) {
+      return res.status(500).send();
     }
   }
 };
